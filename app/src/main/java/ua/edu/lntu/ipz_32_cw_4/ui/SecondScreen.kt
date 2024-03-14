27355f544dbfe8b87d0c53f688d3db6be4399c5d
NazarHorbach99 <nazarhorbach66@gmail.com>
2024-03-14 15:17:09 +0200

    Додана кнопка на SecondScreen Працює з елементом done

--- app/src/main/java/ua/edu/lntu/ipz_32_cw_4/ui/SecondScreen.kt
+++ app/src/main/java/ua/edu/lntu/ipz_32_cw_4/ui/SecondScreen.kt
@@ -1,36 +1,24 @@
 package ua.edu.lntu.ipz_32_cw_4.ui
 
 import androidx.compose.runtime.Composable
-import ua.edu.lntu.ipz_32_cw_4.Screen
-
-
 import androidx.compose.ui.Modifier
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
-
-import androidx.compose.ui.tooling.preview.Preview
-import ua.edu.lntu.ipz_32_cw_4.ui.theme.IPZ_32_cw_4Theme
-import androidx.navigation.compose.NavHost
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+
 @Composable
 fun SecondScreen(modifier: Modifier = Modifier, navController: NavController) {
     Column(modifier = modifier.padding(16.dp)) {
@@ -47,4 +35,3 @@ fun SecondScreen(modifier: Modifier = Modifier, navController: NavController) {
         )
     }
 }
-
